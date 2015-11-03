b4ef2a03edb319a369fb5746c668f0c52b928474
trent <tjmcc01@gmail.com>
2015-11-03 13:44:58 -0800

    adds auth with venmo

--- vpesa.js
+++ vpesa.js
@@ -1,9 +1,36 @@
 if (Meteor.isClient) {
-  
+
+  Template.login.events({
+    'click #login-button': function () {
+      Meteor.loginWithVenmo(function (err) {
+        if (err) {
+          throw new Meteor.Error("Login failed");
+        }
+      });
+    },
+    'click #logout-button': function () {
+      Meteor.logout(function(err){
+        if (err) {
+          throw new Meteor.Error("Logout failed");
+        }
+      });
+    }
+  }); 
+
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+
+    ServiceConfiguration.configurations.upsert({
+      service: "venmo"
+      }, { 
+        $set: {
+        clientId: "3090",
+        scope: "access_profile+access_friends+make_payments",
+        secret: "bA8hM7A3cuAFA2CfT7wawwz497L5MyFp"
+        }
+    });
   });
+
 }
